7298a9aebc0c86ee1dd12b95bff2690c2a7ac792
Magno <jmagno.dutra@hotmail.com>
2025-01-17 23:12:08 -0300

    Two way binding

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,14 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css',
 })
 export class ModalComponent {
   @Output() close = new EventEmitter<void>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
   onClose() {
     this.close.emit();
